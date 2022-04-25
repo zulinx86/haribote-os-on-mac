889118dd02ea93eb56b21e6f865e818a07500ca1
Takahiro Itazuri <zulinx86@gmail.com>
2022-04-25 19:17:02 +0900

    set video mode

--- haribote.asm
+++ haribote.asm
@@ -2,6 +2,11 @@
 
 	org 0xc200
 
+	; Set video mode
+	mov al,0x13		; 320x200 256-color graphics (VGA)
+	mov ah,0x00		; function to set video mode
+	int 0x10		; BIOS interruption for video display
+
 fin:
 	hlt
 	jmp fin

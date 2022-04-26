6f380b83f6ba1b484608f6f8cd75f12bcb495b15
Takahiro Itazuri <zulinx86@gmail.com>
2022-04-26 15:47:55 +0900

    use pointer 2

--- bootpack.c
+++ bootpack.c
@@ -4,11 +4,10 @@ void HariMain(void)
 {
 	int i;
 	char *p;
-
-	for (i = 0x000a0000; i <= 0x000affff; ++i) {
-		p = (char *)i;
-		*p = i & 0x0f;
-	}
+	
+	p = (char *)0x000a0000;
+	for (i = 0; i <= 0xffff; ++i)
+		*(p + i) = i & 0x0f;
 
 	for (;;)
 		io_hlt();

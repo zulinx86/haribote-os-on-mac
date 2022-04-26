956f0bbd09c455a321e870ee7a45b74b8f94e93c
Takahiro Itazuri <zulinx86@gmail.com>
2022-04-26 15:46:34 +0900

    use pointer

--- bootpack.c
+++ bootpack.c
@@ -1,12 +1,14 @@
 void io_hlt(void);
-void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
 	int i;
+	char *p;
 
-	for (i = 0x000a0000; i <= 0x000affff; ++i)
-		write_mem8(i, i & 0x0f);
+	for (i = 0x000a0000; i <= 0x000affff; ++i) {
+		p = (char *)i;
+		*p = i & 0x0f;
+	}
 
 	for (;;)
 		io_hlt();

a05626358196c4d1070a043da35402878af3d36f
Takahiro Itazuri <zulinx86@gmail.com>
2022-04-26 15:43:27 +0900

    paint screen with white

--- bootpack.c
+++ bootpack.c
@@ -1,8 +1,13 @@
 void io_hlt(void);
+void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
-fin:
-	io_hlt();
-	goto fin;
+	int i;
+
+	for (i = 0x000a0000; i <= 0x000affff; ++i)
+		write_mem8(i, 15);
+
+	for (;;)
+		io_hlt();
 }

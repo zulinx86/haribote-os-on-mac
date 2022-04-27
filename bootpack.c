54257cf2ea2cd597e6e46d83e2e5cb1af1aeac66
Takahiro Itazuri <zulinx86@gmail.com>
2022-04-27 22:15:26 +0900

    read a set of bytes from PS/2 mouse

--- bootpack.c
+++ bootpack.c
@@ -9,7 +9,7 @@ void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *)ADDR_BOOTINFO;
 	char s[40], mcursor[256];
-	unsigned char keybuf[32], mousebuf[128];
+	unsigned char keybuf[32], mousebuf[128], mouse_dbuf[3], mouse_phase;
 	int mx, my, i;
 
 	init_gdtidt();
@@ -22,6 +22,7 @@ void HariMain(void)
 
 	fifo8_init(&keyfifo, 32, keybuf);
 	fifo8_init(&mousefifo, 128, mousebuf);
+	mouse_phase = 0;
 	init_keyboard();
 	enable_mouse();
 
@@ -49,9 +50,23 @@ void HariMain(void)
 			} else if (fifo8_status(&mousefifo)) {
 				i = fifo8_get(&mousefifo);
 				io_sti();
-				mysprintf(s, "%02X", i);
-				boxfill(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 48, 32);
-				putfonts(binfo->vram, binfo->scrnx, binfo->fonts, 32, 16, COL8_FFFFFF, s);
+				if (mouse_phase == 0) {
+					if (i == 0xfa)
+						mouse_phase = 1;
+				} else if (mouse_phase == 1) {
+					mouse_dbuf[0] = i;
+					mouse_phase = 2;
+				} else if (mouse_phase == 2) {
+					mouse_dbuf[1] = i;
+					mouse_phase = 3;
+				} else if (mouse_phase == 3) {
+					mouse_dbuf[2] = i;
+					mouse_phase = 1;
+
+					mysprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+					boxfill(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8, 32);
+					putfonts(binfo->vram, binfo->scrnx, binfo->fonts, 32, 16, COL8_FFFFFF, s);
+				}
 			}
 		}
 	}

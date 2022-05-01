b55f1d875577f769a77864ae713ed468435215ca
Takahiro Itazuri <zulinx86@gmail.com>
2022-05-01 11:09:36 +0900

    avoid wraparound

--- sheet.c
+++ sheet.c
@@ -53,6 +53,11 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
 	char *buf, c, *vram = ctl->vram;
 	struct SHEET *sht;
 
+	if (vx0 < 0) vx0 = 0;
+	if (vy0 < 0) vy0 = 0;
+	if (vx1 > ctl->xsize) vx1 = ctl->xsize;
+	if (vy1 > ctl->ysize) vy1 = ctl->ysize;
+
 	for (h = 0; h < ctl->top; ++h) {
 		sht = ctl->sheets[h];
 		buf = sht->buf;

169e87a80ff7759159c76efc90ff5412c6caa087
Takahiro Itazuri <zulinx86@gmail.com>
2022-05-01 11:06:36 +0900

    enable mouse to go rightmost

--- bootpack.c
+++ bootpack.c
@@ -82,8 +82,8 @@ void HariMain(void)
 					my += mdec.y;
 					if (mx < 0) mx = 0;
 					if (my < 0) my = 0;
-					if (mx > binfo->scrnx - 16) mx = binfo->scrnx - 16;
-					if (my > binfo->scrny - 16) my = binfo->scrny - 16;
+					if (mx >= binfo->scrnx) mx = binfo->scrnx - 1;
+					if (my >= binfo->scrny) my = binfo->scrny - 1;
 					mysprintf(s, "(%3d, %3d)", mx, my);
 					boxfill(buf_back, binfo->scrnx, COL8_008484, 0, 0, 80, 16);
 					putfonts(buf_back, binfo->scrnx, binfo->fonts, 0, 0, COL8_FFFFFF, s);

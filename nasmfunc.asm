416b05bf7ea5e91cae6b8a4574d725be3262e799
Takahiro Itazuri <zulinx86@gmail.com>
2022-05-05 21:10:08 +0900

    switch back from task b to task a

--- nasmfunc.asm
+++ nasmfunc.asm
@@ -11,7 +11,7 @@ bits 32
 	global asm_inthandler20, asm_inthandler21, asm_inthandler2c
 	extern inthandler20, inthandler21, inthandler2c
 	global memtest_sub
-	global taskswitch4
+	global taskswitch3, taskswitch4
 
 section .text
 
@@ -182,6 +182,10 @@ memtest_sub:		; unsigned int memtest_sub(unsigned int start, unsigned int end);
 	pop ebx
 	pop esi
 	pop edi
+	ret
+
+taskswitch3:		; void taskswitch3(void);
+	jmp 3*8:0
 	ret
 
 taskswitch4:		; void taskswitch4(void);

af5b8f23ccc93c040d05168ab3f25c0b44309dca
Takahiro Itazuri <zulinx86@gmail.com>
2022-05-05 21:37:52 +0900

    delay displaying count in task b

--- bootpack.c
+++ bootpack.c
@@ -12,7 +12,7 @@ struct TSS32 {
 	int ldtr, iomap;
 };
 
-void task_b_main(void);
+void task_b_main(struct SHEET *sht_back);
 
 void HariMain(void)
 {
@@ -108,7 +108,7 @@ void HariMain(void)
 	set_segmdesc(gdt + 3, 103, (int)&tss_a, AR_TSS32);
 	set_segmdesc(gdt + 4, 103, (int)&tss_b, AR_TSS32);
 	load_tr(3 * 8);
-	task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024;
+	task_b_esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 8;
 	tss_b.eip = (int)&task_b_main;
 	tss_b.eflags = 0x00000202;
 	tss_b.eax = 0;
@@ -125,7 +125,7 @@ void HariMain(void)
 	tss_b.ds = 1 * 8;
 	tss_b.fs = 1 * 8;
 	tss_b.gs = 1 * 8;
-	*((int *)0x0fec) = (int)sht_back;
+	*((int *)(task_b_esp + 4)) = (int)sht_back;
 
 	for (;;) {
 		io_cli();
@@ -272,23 +272,26 @@ void make_textbox(struct SHEET *sht, int x0, int y0, int sx, int sy, int c)
 	boxfill(sht->buf, sht->bxsize, c,           x0 - 1, y0 - 1, x1 + 1, y1 + 1);
 }
 
-void task_b_main(void)
+void task_b_main(struct SHEET *sht_back)
 {
 	struct FIFO32 fifo;
-	struct TIMER *timer_ts;
+	struct TIMER *timer_ts, *timer_put;
 	int i, fifobuf[128], count = 0;
 	char s[11];
-	struct SHEET *sht_back = (struct SHEET *)*((int *)0x0fec);
 
 	fifo32_init(&fifo, 128, fifobuf);
+
 	timer_ts = timer_alloc();
 	timer_init(timer_ts, &fifo, 2);
 	timer_settime(timer_ts, 2);
 
+	timer_put = timer_alloc();
+	timer_init(timer_put, &fifo, 1);
+	timer_settime(timer_put, 1);
+
 	for (;;) {
 		++count;
-		mysprintf(s, "%10d", count);
-		putfonts_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 10);
+		putfonts_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, " ", 1);
 
 		io_cli();
 		if (fifo32_status(&fifo) == 0) {
@@ -297,7 +300,11 @@ void task_b_main(void)
 			i = fifo32_get(&fifo);
 			io_sti();
 
-			if (i == 2) {
+			if (i == 1) {
+				mysprintf(s, "%10d", count);
+				putfonts_sht(sht_back, 0, 144, COL8_FFFFFF, COL8_008484, s, 10);
+				timer_settime(timer_put, 1);
+			} else if (i == 2) {
 				farjmp(0, 3 * 8);
 				timer_settime(timer_ts, 2);
 			}
